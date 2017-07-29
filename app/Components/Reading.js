f669718b0b7368a12bb6cc16fd06484047a95dd4
Peng <rocwang@cityuniversity.edu>
2017-07-28 23:36:42 -0700

    Fixed reading next page button bug

--- app/Components/Reading.js
+++ app/Components/Reading.js
@@ -60,7 +60,7 @@ class Reading extends Component{
 
         {!this.state.readings.length===0 && <div className="rcTitle">There is no reading yet. Please add your reading.</div>}
 
-        {this.state && this.state.readings && this.state.readings.length===0 && <div className="pageBtnDiv text-right">
+        {this.state && this.state.readings && this.state.readings.length!==0 && <div className="pageBtnDiv text-right">
           {this.startNumber>1 && <Link className="btn btn-info loginButton pageBtn" to={{
             pathname: "/reading",
             search: `?start=${this.startNumber*1-5>1?this.startNumber*1-5:1}`
