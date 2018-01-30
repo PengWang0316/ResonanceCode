b99365c94129850edac5e71228b1fee7b8011331
Kevin W <rocwang@cityuniversity.edu>
2018-01-30 15:24:11 -0800

    Changed interfaces to show the new format data.

--- app/Components/DetailedReading.js
+++ app/Components/DetailedReading.js
@@ -3,21 +3,39 @@ import PropTypes from 'prop-types';
 import LineBigram from './LineBigram';
 // import QueryString from "query-string";
 
-const DetailedReading = ({ imageInfos }) => {
+const DetailedReading = ({
+  imageInfos, particleBigram, waveBigram, resonanceBigram, notes
+}) => {
   // componentWillMount(){
   //   let queryInfo=QueryString.parse(this.props.location.search);
   //   console.log("DetailedReading page queryInfo:",queryInfo);
   // }
+  const bigramQuestions = {
+    'Resonance Bigram': resonanceBigram,
+    'Wave Bigram': waveBigram,
+    'Particle Bigram': particleBigram
+  };
   /* Exchanging the order for line 46 and line 25 */
   const newArray = [imageInfos[0], imageInfos[2], imageInfos[1]];
   return (
     <div className="mt-3">
-      {newArray.map(element =>
-        <LineBigram key={element._id} title={element.title} data={element} />)}
+      {newArray.map(element => (
+        <LineBigram
+          key={element._id}
+          title={element.title}
+          question={bigramQuestions[element.title]}
+          data={element}
+        />))}
+      <div className="mt-2 font-weight-bold">Notes:</div>
+      <div>{notes}</div>
     </div>
   );
 };
 DetailedReading.proptypes = {
-  imageInfos: PropTypes.array.isRequired
+  imageInfos: PropTypes.array.isRequired,
+  particleBigram: PropTypes.string.isRequired,
+  waveBigram: PropTypes.string.isRequired,
+  resonanceBigram: PropTypes.string.isRequired,
+  notes: PropTypes.string.isRequired
 };
 export default DetailedReading;
