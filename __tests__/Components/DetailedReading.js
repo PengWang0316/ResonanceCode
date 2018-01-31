21c95b174143890d079ef82a73d56d4ec3d3a669
Kevin W <rocwang@cityuniversity.edu>
2018-01-30 23:47:38 -0800

    Updated two components' snapshots.

--- __tests__/Components/DetailedReading.js
+++ __tests__/Components/DetailedReading.js
@@ -6,7 +6,12 @@ import DetailedReading from '../../app/Components/DetailedReading';
 jest.mock('../../app/Components/LineBigram', () => 'LineBigram');
 
 describe('DetailedReading test', () => {
-  const defaultProps = { imageInfos: [{ _id: '1', title: 'title1' }, { _id: '2', title: 'title2' }, { _id: '3', title: 'title3' }] };
+  const defaultProps = {
+    imageInfos: [{ _id: '1', title: 'title1' }, { _id: '2', title: 'title2' }, { _id: '3', title: 'title3' }],
+    hexagram: {
+      resonance_bigram_question: 'resonance_bigram_question', wave_bigram_question: 'wave_bigram_question', paticle_bigram_question: 'paticle_bigram_question', notes: 'notes'
+    }
+  };
 
   test('DetailedReading snapshot', () => expect(renderer.create(<DetailedReading {...defaultProps} />).toJSON()).toMatchSnapshot());
 });
