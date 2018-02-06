d5b079cbba8f523d5045218bd6daad9a65c22bdf
Kevin W <rocwang@cityuniversity.edu>
2018-02-06 15:54:48 -0800

    Using hexagram icon to replace background color changing. Also removed taiji icon. Snapshot was updated.

--- app/Components/BigramBlockSmall.js
+++ app/Components/BigramBlockSmall.js
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from '../styles/BigramBlockSmall.module.css';
-import taijiSmall from '../imgs/taijiSmall.png';
+// import taijiSmall from '../imgs/taijiSmall.png';
 
 const BigramBlockSmall = ({ lineText, position }) => (
   <div className="mr-4">
+    {/*
     <img className={styles.taijiPic} src={taijiSmall} alt="taiji" />
+    */}
     <div className="d-flex">
-      <div className={`${styles.bigramBlock} ${styles.blockBottomBorder}  ${styles.blockRightBorder} ${position === 1 ? styles.selected : ''}`} /><div className={`${styles.bigramBlock} ${styles.blockBottomBorder} ${position === 0 ? styles.selected : ''}`} />
+      <div className={`${styles.bigramBlock} ${styles.blockBottomBorder}  ${styles.blockRightBorder} ${position === 1 ? styles.growth : ''}`} /><div className={`${styles.bigramBlock} ${styles.blockBottomBorder} ${position === 0 ? styles.conception : ''}`} />
     </div>
     <div className="d-flex">
-      <div className={`${styles.bigramBlock} ${styles.blockRightBorder} ${position === 2 ? styles.selected : ''}`} /><div className={`${styles.bigramBlock} ${position === 3 ? styles.selected : ''}`} />
+      <div className={`${styles.bigramBlock} ${styles.blockRightBorder} ${position === 2 ? styles.maturation : ''}`} /><div className={`${styles.bigramBlock} ${position === 3 ? styles.seeding : ''}`} />
     </div>
     <div className={styles.lineText}>{lineText}</div>
   </div>
