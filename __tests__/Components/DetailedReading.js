9fed0ba09b01ac27e65f906069afaea9dd0470b6
Kevin W <rocwang@cityuniversity.edu>
2018-02-10 20:53:12 -0800

    Updated test code for DetailedReading component.

--- __tests__/Components/DetailedReading.js
+++ __tests__/Components/DetailedReading.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import DetailedReading from '../../app/Components/DetailedReading';
 
@@ -12,9 +13,18 @@ describe('DetailedReading test', () => {
   const defaultProps = {
     imageInfos: [{ _id: '1', title: 'title1' }, { _id: '2', title: 'title2' }, { _id: '3', title: 'title3' }],
     hexagram: {
-      resonance_bigram_question: 'resonance_bigram_question', wave_bigram_question: 'wave_bigram_question', paticle_bigram_question: 'paticle_bigram_question', analysis: 'analysis', question: 'question', overview: 'overview'
-    }
+      resonance_bigram_question: 'resonance_bigram_question', wave_bigram_question: 'wave_bigram_question', paticle_bigram_question: 'paticle_bigram_question', analysis: 'analysis', question: 'question', overview: 'overview', number: 1
+    },
+    handleHexagramClick: jest.fn()
   };
 
+  const getShallowComponent = () => shallow(<DetailedReading {...defaultProps} />);
+
   test('DetailedReading snapshot', () => expect(renderer.create(<DetailedReading {...defaultProps} />).toJSON()).toMatchSnapshot());
+
+  test('handleHexagramClick', () => {
+    const component = getShallowComponent();
+    component.find({ id: 1 }).simulate('click');
+    expect(defaultProps.handleHexagramClick).toHaveBeenCalledTimes(1);
+  });
 });
