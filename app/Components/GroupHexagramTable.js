d3237f269bb8b979ad79f976bdd305447aea08db
Kevin W <rocwang@cityuniversity.edu>
2018-02-12 22:45:17 -0800

    Added GroupHexagramRow and GroupHexagramTable components.

---
+++ app/Components/GroupHexagramTable.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import GroupHexagramRow from './GroupHexagramRow';
+import styles from '../styles/GroupHexagramTable.module.css';
+
+const GroupHexagramTable = ({ hexagramArray, handleHexagramClick }) => (
+  <div>
+    <div className="font-weight-bold mt-3 mb-2">Resonance Quartet Hexagrams</div>
+    <table className={styles.table}>
+      <thead>
+        <tr className={styles.headTR}>
+          <th>Potentiation</th>
+          <th>Hex No</th>
+          <th>Hex Image</th>
+          <th>RC Name</th>
+        </tr>
+      </thead>
+      <tbody className={styles.tbody}>
+        {hexagramArray.map(hexagram => (
+          <GroupHexagramRow
+            key={hexagram.name}
+            hexagram={hexagram}
+            handleHexagramClick={handleHexagramClick}
+          />))}
+      </tbody>
+    </table>
+  </div>
+);
+GroupHexagramTable.propTypes = {
+  hexagramArray: PropTypes.array.isRequired,
+  handleHexagramClick: PropTypes.func.isRequired
+};
+export default GroupHexagramTable;
