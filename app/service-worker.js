d4287e2bcc4aead4d0fe6cb7aa32ccaa3c2727d2
Kevin W <rocwang@cityuniversity.edu>
2018-02-19 13:53:38 -0800

    Resolved #46 Some improvements (#68)
    * Added lang attribute to html element.
    
    * Moved the jQuery labrary to the bottom in order to speed up the first reaction of the page.
    
    * Cleared up index.html file.
    
    * Optimized some pictures size.
    
    * Clearing up
    
    * Optimized the size of some images.
    
    * Optimized lancher icons.
    
    * Changed the loading process of css file to a unblocked way. Removed a google font css file from index.html. Removed cache code for the google font.
    
    * Switched node js to http 2.

--- app/service-worker.js
+++ app/service-worker.js
@@ -37,7 +37,7 @@ const thirdPartyLibraryStrategy = workboxSW.strategies.cacheFirst({
 /* Setting up the router for some third party libraries. */
 workboxSW.router.registerRoute('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', thirdPartyLibraryStrategy);
 workboxSW.router.registerRoute(/^https:\/\/use.fontawesome.com\/releases\/v5.0.1\/.*/, thirdPartyLibraryStrategy);
-workboxSW.router.registerRoute('https://fonts.googleapis.com/css?family=Quicksand:300', thirdPartyLibraryStrategy);
+// workboxSW.router.registerRoute('https://fonts.googleapis.com/css?family=Quicksand:300', thirdPartyLibraryStrategy);
 workboxSW.router.registerRoute('https://fonts.googleapis.com/css?family=Caveat', thirdPartyLibraryStrategy);
 workboxSW.router.registerRoute('https://code.jquery.com/jquery-3.2.1.slim.min.js', thirdPartyLibraryStrategy);
 workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', thirdPartyLibraryStrategy);
