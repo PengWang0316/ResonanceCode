c39db0f1ba15baf12cd6d575b24995f08334da20
Kevin W <rocwang@cityuniversity.edu>
2018-02-24 00:37:45 -0800

    Merged and organized code.

--- app/Components/HexagramDetailModal.js
+++ app/Components/HexagramDetailModal.js
@@ -13,7 +13,6 @@ import { fetchHexagrams, clearHexagrams } from '../actions/HexagramActions';
 import { TOTAL_NUMBER_HEXAGRAM } from '../config';
 import HexagramListContainer from './containers/HexagramListContainer';
 
-<<<<<<< HEAD
 /* The component shows the detailed information for hexagram */
 export class HexagramDetailModal extends Component {
 
@@ -28,18 +27,6 @@ export class HexagramDetailModal extends Component {
   static defaultProps = {
     hexagramArr: ''
     // hexagramsImgArrMap: {}
-=======
-class HexagramDetailModal extends Component {
-
-  static propTypes = {
-    hexagram: PropTypes.object,
-    handleHexagramClick: PropTypes.func.isRequired,
-    hexagramsImgArrMap: PropTypes.object
-  };
-  static defaultProps = {
-    hexagram: null,
-    hexagramsImgArrMap: {}
->>>>>>> 3f805ca8e945601db509c6aa0c77c68ba0ab3485
   };
 
   static getGroupHexagramObject = (name, imageArr) => ({
@@ -59,7 +46,6 @@ class HexagramDetailModal extends Component {
     HexagramDetailModal.getGroupHexagramObject('Hidden Influence', hexagram.hidden_influence_hexagram)
   ];
 
-<<<<<<< HEAD
   /**
    * Creating a hexagrams object with img_arr keys.
    * @param {array} hexagrams is the array has all hexagram.
@@ -116,10 +102,6 @@ class HexagramDetailModal extends Component {
     this.setState({ hexagram: HexagramListContainer.getHexagramBaseOnTarget(target, 'number', this.hexagramNumbersMap) });
   }
 
-=======
-  state = { hexagram: null };
-
->>>>>>> 3f805ca8e945601db509c6aa0c77c68ba0ab3485
   render() {
     return (
       <div className="modal fade bd-example-modal-lg" id="hexagramDetailModal" tabIndex="-1" role="dialog" aria-labelledby="hexagramDetailModalLabel" aria-hidden="true">
@@ -131,24 +113,15 @@ class HexagramDetailModal extends Component {
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-<<<<<<< HEAD
             {this.state.hexagram &&
               <div className="modal-body">
                 <ImageDescription
                   imageInfo={this.state.hexagram}
                   imageNumber={this.state.hexagram.img_arr}
-=======
-            {this.props.hexagram &&
-              <div className="modal-body">
-                <ImageDescription
-                  imageInfo={this.props.hexagram}
-                  imageNumber={this.props.hexagram.img_arr}
->>>>>>> 3f805ca8e945601db509c6aa0c77c68ba0ab3485
                   isFirstImage
                   isBlack
                 />
                 <div className="mt-4 d-flex flex-wrap justify-content-center">
-<<<<<<< HEAD
                   <BigramClockBig lineText="Resonance Line 2/5" position={this.state.hexagram.line_25} />
                   <BigramClockBig lineText="Agency/Process Line 1/4" position={this.state.hexagram.line_14} />
                   <BigramClockBig lineText="Agent/Identity Line 3/6" position={this.state.hexagram.line_36} />
@@ -188,47 +161,6 @@ class HexagramDetailModal extends Component {
                   handleHexagramClick={this.handleAssociatedHexagramClick}
                   tableTitle="Resonance Quartet Hexagrams"
                   hexagramsImgArrMap={this.hexagramsImgArrMap}
-=======
-                  <BigramClockBig lineText="Resonance Line 2/5" position={this.props.hexagram.line_25} />
-                  <BigramClockBig lineText="Agency/Process Line 1/4" position={this.props.hexagram.line_14} />
-                  <BigramClockBig lineText="Agent/Identity Line 3/6" position={this.props.hexagram.line_36} />
-                </div>
-                <div className="pl-4 mt-5 d-flex flex-wrap justify-content-around">
-                  <BigramBlockBig lineText="Particle Bigram: Line 1 and 3" position={this.props.hexagram.line_13} isLine13 />
-                  <BigramBlockBig lineText="Wave Bigram: Line 4 and 6" position={this.props.hexagram.line_46} />
-                </div>
-                <div className={`mt-4 ${styles.imagePoetryDiv}`}>
-                  {this.props.hexagram.image &&
-                    <div className={`float-sm-left ${styles.imageDiv}`}>
-                      <img src={this.props.hexagram.image} alt="this.props.hexagram" className={`img-fluid ${styles.image}`} />
-                      <div className={styles.imageCitation}>{this.props.hexagram.image_citation}</div>
-                    </div>}
-                  <blockquote
-                    className={styles.poetry}
-                    style={this.props.hexagram.poetry_font_size ? { fontSize: `${this.props.hexagram.poetry_font_size}px` } : {}}
-                  >
-                    {this.props.hexagram.poetry}
-                  </blockquote>
-                </div>
-                <div className={`mt-4 ${styles.overViewDiv}`}>Overview:</div>
-                <div className={styles.preLineWhiteSpace}>{this.props.hexagram.overview}</div>
-                <div className="mt-4 font-weight-bold">Analysis:</div>
-                <div className={`mb-4 ${styles.preLineWhiteSpace}`}>{this.props.hexagram.analysis}</div>
-                <div className="mt-4 font-weight-bold">Question:</div>
-                <div className={styles.preLineWhiteSpace}>{this.props.hexagram.question}</div>
-                <ChangingLines hexagram={this.props.hexagram} hexagramsImgArrMap={this.props.hexagramsImgArrMap} />
-                <GroupHexagramTable
-                  hexagramArray={HexagramDetailModal.getAssociateHexagramArray(this.props.hexagram)}
-                  handleHexagramClick={this.props.handleHexagramClick}
-                  tableTitle="Associated Hexagrams"
-                  hexagramsImgArrMap={this.props.hexagramsImgArrMap}
-                />
-                <GroupHexagramTable
-                  hexagramArray={HexagramDetailModal.getQuartetHexagramArray(this.props.hexagram)}
-                  handleHexagramClick={this.props.handleHexagramClick}
-                  tableTitle="Resonance Quartet Hexagrams"
-                  hexagramsImgArrMap={this.props.hexagramsImgArrMap}
->>>>>>> 3f805ca8e945601db509c6aa0c77c68ba0ab3485
                 />
               </div>
             }
@@ -242,7 +174,6 @@ class HexagramDetailModal extends Component {
   }
 }
 
-<<<<<<< HEAD
 /* istanbul ignore next */
 const mapStateToProps = state => ({ hexagrams: state.hexagrams });
 /* istanbul ignore next */
@@ -251,7 +182,3 @@ const mapDispatchToProps = dispatch => ({
   clearHexagrams: () => dispatch(clearHexagrams())
 });
 export default connect(mapStateToProps, mapDispatchToProps)(HexagramDetailModal);
-=======
-
-export default HexagramDetailModal;
->>>>>>> 3f805ca8e945601db509c6aa0c77c68ba0ab3485
