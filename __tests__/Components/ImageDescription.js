c60233b2879885e6efe95c7126ccc12d0a256d03
Kevin W <rocwang@cityuniversity.edu>
2018-03-05 13:11:22 -0800

    Added more test code for the ImageDescription component.

--- __tests__/Components/ImageDescription.js
+++ __tests__/Components/ImageDescription.js
@@ -21,5 +21,18 @@ describe('ImageDescription test', () => {
   };
   // const getShallowComponent = (props = defaultProps) => shallow(< {...props} />);;
 
-  test('ImageDescription snapshot', () => expect(renderer.create(<ImageDescription {...defaultProps} />).toJSON()).toMatchSnapshot());
+  test('ImageDescription has resonance code name snapshot', () => expect(renderer.create(<ImageDescription {...{
+    ...defaultProps,
+    imageInfo: {
+      image_text: 'imageText',
+      number: 'number',
+      chinese_name: 'chineseName',
+      wilhelm_huang_hintley_name: 'wilhelm_huang_hintley_name',
+      rc_description: 'rc_description',
+      resonance_code_name: 'new rc name'
+    }
+  }}
+  />).toJSON()).toMatchSnapshot());
+
+  test('ImageDescription has no resonance code name snapshot', () => expect(renderer.create(<ImageDescription {...defaultProps} />).toJSON()).toMatchSnapshot());
 });
