68c451c4aea1583fa8cac12879e44f7152d469b5
Kevin W <rocwang@cityuniversity.edu>
2018-03-08 18:09:01 -0800

    Added istanbul ignore next comment for redux connect function.

--- __tests__/Components/SharedComponents/LoadingAnimation.js
+++ __tests__/Components/SharedComponents/LoadingAnimation.js
@@ -8,15 +8,7 @@ describe('LoadingAnimation test', () => {
   const defaultProps = {
     isLoading: true
   };
-  // const getShallowComponent = (props = defaultProps) => shallow(<LoadingAnimation {...props} />);
-
-<<<<<<< HEAD
   test('LoadingAnimation is loading snapshot', () => expect(renderer.create(<LoadingAnimation {...defaultProps} />).toJSON()).toMatchSnapshot());
 
   test('LoadingAnimation is not loading snapshot', () => expect(renderer.create(<LoadingAnimation isLoading={false} />).toJSON()).toMatchSnapshot());
-=======
-  test('LoadingAnimation isLoading snapshot', () => expect(renderer.create(<LoadingAnimation {...defaultProps} />).toJSON()).toMatchSnapshot());
-
-  test('LoadingAnimation not isLoading snapshot', () => expect(renderer.create(<LoadingAnimation isLoading={false} />).toJSON()).toMatchSnapshot());
->>>>>>> 01d89d95cd9adb97c92c6c974c6582dd77f2dda1
 });
--- app/Components/SharedComponents/LoadingAnimation.js
+++ app/Components/SharedComponents/LoadingAnimation.js
@@ -14,5 +14,6 @@ export const LoadingAnimation = props => {
 LoadingAnimation.propTypes = {
   isLoading: PropTypes.bool.isRequired
 };
+/* istanbul ignore next */
 const mapStateToProps = state => ({ isLoading: state.isLoading });
 export default connect(mapStateToProps, null)(LoadingAnimation);
