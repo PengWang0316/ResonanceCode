91afc965b216a052998635d7f7a789e7b0c0c43e
Kevin W <rocwang@cityuniversity.edu>
2018-03-23 22:05:08 -0700

    Fixed giving date string will fail to run in the machine that is in the different time zone.

--- __tests__/Components/Containers/AddReadingContainer.js
+++ __tests__/Components/Containers/AddReadingContainer.js
@@ -234,7 +234,7 @@ describe('AddReadingContainer test', () => {
         changeLinesNumberArray: ['1', '4']
       }
     });
-    component.find('AddReadingForm').prop('handleSubmit')({ readingName: 'readingName', date: '02/27/2018', people: 'people' });
+    component.find('AddReadingForm').prop('handleSubmit')({ readingName: 'readingName', date: '2018-02-27T08:00:00.000Z', people: 'people' });
     expect(defaultProps.createReading).toHaveBeenLastCalledWith({
       change_lines: ['1', '4'], change_lines_text: '', date: new Date('2018-02-27T08:00:00.000Z'), hexagram_arr_1: '7,9-6,8-7,9-6,8-7,9', hexagram_arr_2: '7,9-7,9-7,9-6,8-6,8', img1: '7,6,7,6,7', img2: '7,7,7,6,6', people: 'people', reading_name: 'readingName', userName: 'displayName'
     });
@@ -271,7 +271,7 @@ describe('AddReadingContainer test', () => {
       user: { customName: 'customerName' }
     });
     // const component = getShallowComponent({ ...defaultProps, user: { customName: 'customerName' } });
-    component.find('AddReadingForm').prop('handleSubmit')({ readingName: 'readingName', date: '02/27/2018', people: 'people' });
+    component.find('AddReadingForm').prop('handleSubmit')({ readingName: 'readingName', date: '2018-02-27T08:00:00.000Z', people: 'people' });
     expect(defaultProps.createReading).toHaveBeenLastCalledWith({
       change_lines: ['1', '4'], change_lines_text: '', date: new Date('2018-02-27T08:00:00.000Z'), hexagram_arr_1: '7,9-6,8-7,9-6,8-7,9', hexagram_arr_2: '7,9-7,9-7,9-6,8-6,8', img1: '7,6,7,6,7', img2: '7,7,7,6,6', people: 'people', reading_name: 'readingName', userName: 'customerName'
     });
