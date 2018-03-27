eeed80fd7944756a7606448ffc3030f7577d3993
Kevin W <rocwang@cityuniversity.edu>
2018-03-26 21:47:35 -0700

    Added the new GirgramCircle componnet.

---
+++ app/Components/BigramCircle.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from '../styles/BigramCircle.module.css';
+import growthImage from '../imgs/growth.png';
+import conceptionImage from '../imgs/conception.png';
+import seedingImage from '../imgs/seeding.png';
+import maturationImage from '../imgs/maturation.png';
+
+const BigramCircle = ({ circlePosition, linePoint }) => {
+  // Start to calculate styles for divs
+  let {
+    firstDiv, secondDiv, thirdDiv, fourthDiv
+  } = styles;
+  switch (linePoint) {
+    case 0:
+      firstDiv = `${firstDiv} ${styles.emphasizedZero}`;
+      break;
+    case 1:
+      fourthDiv = `${fourthDiv} ${styles.emphasizedOne}`;
+      break;
+    case 2:
+      thirdDiv = `${thirdDiv} ${styles.emphasizedTwo}`;
+      firstDiv = `${firstDiv} ${styles.cancelBottomBorder}`;
+      fourthDiv = `${fourthDiv} ${styles.cancelLeftBorder}`;
+      break;
+    default:
+      firstDiv = `${firstDiv} ${styles.cancelRightBorder}`;
+      fourthDiv = `${fourthDiv} ${styles.cancelTopBorder}`;
+      secondDiv = `${secondDiv} ${styles.emphasizedThree}`;
+      break;
+  }
+  return (
+    <div className={styles[`circle-${circlePosition}`]}>
+      <div className={`${styles.internalDiv} d-flex`}>
+        <div className={firstDiv}>
+          <img className="ml-2 mt-2" style={{ opacity: linePoint === 0 ? 1 : 0.4 }} src={conceptionImage} alt="conception" />
+        </div>
+        <div className={secondDiv}>
+          <img className="mr-2 mt-2" style={{ opacity: linePoint === 3 ? 1 : 0.4 }} src={seedingImage} alt="seeding" />
+        </div>
+      </div>
+      <div className={`${styles.internalDiv} d-flex`}>
+        <div className={thirdDiv}>
+          <img className="ml-2 mb-2" style={{ opacity: linePoint === 2 ? 1 : 0.4 }} src={maturationImage} alt="maturation" />
+        </div>
+        <div className={fourthDiv}>
+          <img className="mr-2 mb-2" style={{ opacity: linePoint === 1 ? 1 : 0.4 }} src={growthImage} alt="growth" />
+        </div>
+      </div>
+    </div>
+  );
+};
+BigramCircle.propTypes = {
+  circlePosition: PropTypes.string.isRequired,
+  linePoint: PropTypes.number.isRequired
+};
+export default BigramCircle;
